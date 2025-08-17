ec48468efd2f51d046b22080eec246d8c6b9ac2a
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-08-17 18:23:50 +0800

    activity6.py

---
+++ activity6.py
@@ -0,0 +1,11 @@
+n1 = eval(input("enter first number:"))
+n2 = eval(input("enter second number:"))
+s = n1 + n2
+d= n1 - n2
+p = n1 * n2
+q = n1 / n2
+
+print("the sum of",n1,"and",n2, "is equal to",s)
+print("the difference of",n1,"and",n1,"is equal to",d)
+print("the product of",n1,"and",n2,"isequal to",p)
+print("the quotient of",n1,"and",n2,"is equal to",q)

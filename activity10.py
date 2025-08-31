ef8ddd620a0fc4aa3cb5c44d18b2d7ea61f7348c
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-08-31 09:30:07 +0800

    activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = 'mark'
+password = 'neji'
+
+u = input("USERNAME --> ")
+p = input("PASSWORD --> ")
+
+if (u == username) and (p == password):
+	print("Access granted")
+else:
+	print("Acces Denied"

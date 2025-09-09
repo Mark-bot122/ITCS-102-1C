4022317a244dc00026c27025cafd2d9ba16624b9
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-09-09 14:47:39 +0800

    code_challenge3.py

---
+++ code_challenge3.py
@@ -0,0 +1,18 @@
+u = input("What is your name? ")
+f = input("How much is your fare? ")
+s = input("Are you currently a student? ")
+
+username = u
+fare = f
+student = s
+
+if (username == u):
+	print("HI " ,username)
+
+if (fare == f):
+	print("Your given fare is ", fare)
+
+if (student == s):
+	print("Your discount is 20%,which leaves you", )
+else:
+	print("Unfortunately,your not discounted")

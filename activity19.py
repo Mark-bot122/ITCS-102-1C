dbb4d74656ff45159572b041e528f04ea780bce7
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-11-12 13:21:53 +0800

    Add pattern printing logic in activity19.py
    Initial implementation of a pattern printing program.

---
+++ activity19.py
@@ -0,0 +1,19 @@
+
+print("\t\t *", end= " ")
+for x in range(1,11,1):
+    for y in range(10,x,-1):
+        print(" ", end= " ")
+    for z in range(1,x,1):
+        print("*", end= " ")
+    for m in range(1,x,1):
+        print("*", end= " ")
+    print()
+
+for l in range(1,11,1):
+    for o in range(1,l,-1):
+        print(" ", end= " ")
+    for q in range(11,l,-1):
+        print("*", end= " ")
+    for t in range():
+        print()
+    print()

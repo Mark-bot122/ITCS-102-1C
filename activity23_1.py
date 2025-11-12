254f0a6cdf597ba818fcc2d809865d9f9a953fa1
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-11-12 14:08:11 +0800

    Add greeting and calculation functions

--- activity23_1.py
+++ activity23_1.py
@@ -1,2 +1,20 @@
 def greetwithname(name):
     print(f"HI {name}, I Hope you're having a good day!!")
+
+def greetperson(name,loc,age):
+    print(f"Hi {name}, from {loc} and {age} year\'s old")
+
+def functionwithreturn(number):
+    print(f"This function calculates the summation from 1 - {number}")
+    sum = 0
+    for x in range(1, number+1, 1):
+        sum += x
+    return sum
+
+def factorialwithreturn(number):
+    print(f"This function calculates the factorial from 1 - {number}")
+    fac = 1
+    for z in range(number,0, -1):
+        fac *= z
+    return fac
+    

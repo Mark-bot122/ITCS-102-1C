5d539db25eb6bcbb84cb1ba6373a54ebd60b8327
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-11-12 13:22:20 +0800

    Add loop to prompt for washing clothes again

---
+++ activity20.py
@@ -0,0 +1,11 @@
+isDirty = True
+
+while isDirty == True:
+    wash_again = input("Continue washing the clothes ( yes / no ): ").lower()
+
+    if wash_again == 'yes':
+        print("Washing the clothes again... ")
+        continue
+    else:
+        print("washing stops now...")
+        break

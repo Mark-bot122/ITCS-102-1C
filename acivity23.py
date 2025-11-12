5e95842af4b3c503809fba73fc9e4f478fa71862
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-11-12 13:36:36 +0800

    Add firstfunction and greetwithname calls

---
+++ acivity23.py
@@ -0,0 +1,11 @@
+from activity23_1 import greetwithname
+
+def firstfunction():
+    print("Lmao")
+    print("Nagkaon na ba?")
+    print("Kekw")
+
+firstfunction()
+greetwithname("Neji")
+greetwithname("Kumalala")
+

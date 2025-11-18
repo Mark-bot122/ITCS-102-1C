7a6304e01f01813ca6cdae4748e3eca07448ee5b
Nejicodes09 <72973784+Mark-bot122@users.noreply.github.com>
2025-11-18 14:32:36 +0800

    Create activity24(WIP).py

---
+++ activity24(WIP).py
@@ -0,0 +1,14 @@
+from activity23_1 import *
+
+
+print("WELCOME TO MY COMPILER PROGRAM")
+name = input("What is your name? ")
+
+print(f"HI,{name}, please select from the option below:")
+print("A - greet name / B. Greet name with age and location, ")
+
+isCont == True
+
+while isCont == True:
+
+    choice = input("Select from A - C: ")
